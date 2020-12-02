99a3ea201de86b000245356f1ce61f69e00f7134
Jelili Noibi <drealjstar@yahoo.com>
2020-12-02 17:12:21 +0100

    format app.js component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import Clip from './Component/clip';
-import './App.css';
+import React from "react";
+import Clip from "./Component/clip";
+import "./App.css";
 
 function App() {
   return (
-    <div className ='App'>
-      
-  <Clip />
-   
-
-    </div>
+      <div className="App">
+        <Clip />
+      </div>
   );
 }
 
