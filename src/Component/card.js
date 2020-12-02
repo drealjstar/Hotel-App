16d1140890b21d72f2e6ef1b48918825c3d0dc2d
Jelili Noibi <drealjstar@yahoo.com>
2020-12-02 17:10:42 +0100

    format some pages for readabilty

--- src/Component/card.js
+++ src/Component/card.js
@@ -1,39 +1,32 @@
 import React, { Component } from "react";
-import MovingText from './movingtext';
+import MovingText from "./movingtext";
 import "./card.css";
 
-
 class Card extends Component {
-
   render() {
-  const prices = [
-  
-    {
-     time : 'Par Hour',
-     amount : 10
-    },
-   
-    {
-      time : '12 Hours',
-      amount : 150
-    },
-  
-    {
-     time : '24 Hours',
-     amount : 300
-    }
-  ]
+    const prices = [
+      {
+        time: "Par Hour",
+        amount: 10,
+      },
 
+      {
+        time: "12 Hours",
+        amount: 150,
+      },
 
-
+      {
+        time: "24 Hours",
+        amount: 300,
+      },
+    ];
 
     return (
       <div>
-   
-       <div className="flip-card-wrap">
-        {prices.map(price => {
-             return(
-             <div className="flip-card-inner">
+        <div className="flip-card-wrap">
+          {prices.map((price) => {
+            return (
+              <div className="flip-card-inner">
                 <div className="card-front">
                   <h1>TO LODGE</h1>
                   <h2>Roll Over For</h2>
@@ -42,25 +35,18 @@ class Card extends Component {
                   <h1>Dollar</h1>
                 </div>
                 <div className="card-back">
-             <h2>{price.time}</h2>
-             <p>  {price.amount}</p>
-
+                  <h2>{price.time}</h2>
+                  <p> {price.amount}</p>
                 </div>
-              </div>);
+              </div>
+            );
+          })}
+        </div>
 
-        })}
-
-
-
-
+        <MovingText />
       </div>
-    
-      <MovingText />
-
-    </div>
     );
   }
 }
 
 export default Card;
-
--- src/Component/movingtext.js
+++ src/Component/movingtext.js
@@ -2,26 +2,18 @@ import React, { Component } from "react";
 import Photo from "./photo";
 import "./movingtext.css";
 
+class MovingText extends Component {
+  render() {
+    return (
+      <div class="text-box">
+        <h1 class="text">
+          <span class="typewriter-text">View Rooms Pictures Below ...</span>
+        </h1>
 
-class MovingText extends Component{
-    render(){
-        return(
-            <div class= "text-box">
-
-<h1 class= "text">
-  <span class="typewriter-text">
-    View Rooms Pictures Below ...
-  </span>  
-</h1>  
-
-         <Photo />
-          
-            </div>
-        )
-    }
-    
+        <Photo />
+      </div>
+    );
+  }
 }
-
-
 
 export default MovingText;
